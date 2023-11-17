355b48a08a9ca9a2ba129a50d8b4f0340e90d3a9
deyvid lins <deyvidg.lins@gmail.com>
2023-11-16 21:48:59 -0300

    Operadores com Template

---
+++ JavaScript/operadores.js
@@ -0,0 +1,8 @@
+function operacoesBasicas(numero1, numero2) {
+    console.log(`${numero1} + ${numero2} = ${numero1 + numero2}`);
+    console.log(`${numero1} - ${numero2} = ${numero1 - numero2}`);
+    console.log(`${numero1} x ${numero2} = ${numero1 * numero2}`);
+    console.log(`${numero1} / ${numero2} = ${numero1 / numero2}`);
+}
+
+operacoesBasicas(4, 5);
